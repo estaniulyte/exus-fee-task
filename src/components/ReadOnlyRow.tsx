55ffc726e4dc7dc3ddfec3cc5d0ae34833e1e81f
Evelina Staniulytė <e.staniulyte98@gmail.com>
2023-05-19 18:28:03 +0300

    style: add disabled button styling

--- src/components/ReadOnlyRow.tsx
+++ src/components/ReadOnlyRow.tsx
@@ -1,9 +1,20 @@
-function SideBar() {
+function ReadOnlyRow() {
   return (
-    <div>
-      Read Only row
-    </div>
+    <tr className="hover:bg-gray-50" key={item.id}>
+      <td className="px-6 py-4 text-gray-700">
+        {item.id}
+      </td>
+      <td className="px-6 py-4 text-gray-700">
+        {item.name}
+      </td>
+      <td className="px-6 py-4 text-gray-700">
+        {item.email}
+      </td>
+      <td className="px-6 py-4 text-gray-700">
+        {item.organizationId}
+      </td>
+    </tr>
   )
 }
 
-export default SideBar
+export default ReadOnlyRow
